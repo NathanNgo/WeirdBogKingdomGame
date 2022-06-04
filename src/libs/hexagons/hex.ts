8d2e667dd9faf311446f7079dce067622597fd20
NathanNgo <nathanj238@gmail.com>
2022-06-04 18:34:42 +1000

    Restructured Project

--- src/libs/hexagons/hex.ts
+++ src/libs/hexagons/hex.ts
@@ -3,6 +3,7 @@ class Hex {
     q: number;
     r: number;
     s: number;
+    type: string;
 
     /**
      * Creates a new Hex object.
@@ -10,10 +11,12 @@ class Hex {
      * @param q - The first co-ordinate.
      * @param r - The second co-ordinate.
      * @param s - The third co-ordinate.
+     * @param type - The type of the hex.
      */
-    constructor(q: number, r: number, s: number) {
+    constructor(q: number, r: number, s: number, type: string) {
         this.q = q;
         this.r = r;
         this.s = s;
+        this.type = type;
     }
 }
